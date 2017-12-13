298ebbc3c6b40f6801d4d2f4bd3f484697fe8395
rezaroshany76 <34510391+rezaroshany76@users.noreply.github.com>
2017-12-13 16:02:50 +0330

    Add files via upload

---
+++ factoryel.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int  vorodi,f=1,i;
+	cin>>vorodi;
+	for(i=1;i<=vorodi;i++)
+	{
+		f=f*i;
+	}
+	cout<<f;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+	
+}
